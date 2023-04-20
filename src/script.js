51b6586c33a0d9c9bb10420efd6d33cad7680ff2
Arkadiy <woodythps@gmail.com>
2023-04-21 01:03:15 +0300

    upd

--- src/script.js
+++ src/script.js
@@ -1,126 +1,96 @@
-function splitText(text) {
-  const words = text.split(/\s+/);
-  return words;
-}
-
-let currentWordIndex = 0;
-let intervalId = null;
-let words = [];
+let currentIndex = 0;
+let currentTextIndex = 0;
+let intervalId;
+let timerIntervalId;
 let wordCount = 0;
-let startTime = null;
-let elapsedTime = null;
-let wpm = 0;
-let timerId = null;
+let startTime = new Date();
 
-function displayNextWord() {
-  const wordContainer = document.querySelector(".word-container");
-  wordContainer.textContent = words[currentWordIndex];
-
-  currentWordIndex++;
-  if (currentWordIndex >= words.length) {
-    currentWordIndex = 0;
-  }
-
-  wordCount++;
-  updateStats();
-}
+const texts = ["rem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor", "Sit amet consectetur adipiscing elit"];
 
 function updateTimer() {
-  const timerElement = document.querySelector(".timer");
-  const elapsedSeconds = Math.floor((Date.now() - startTime) / 1000);
-  const minutes = Math.floor(elapsedSeconds / 60);
-  const seconds = elapsedSeconds % 60;
-  timerElement.textContent = `${minutes}:${seconds
-    .toString()
-    .padStart(2, "0")}`;
+  const currentTime = new Date();
+  const totalTime = (currentTime - startTime) / 1000;
+  document.getElementById("timer").innerText = `Время: ${Math.round(totalTime)} секунд`;
 }
 
-function startReading(speed) {
-  stopReading();
-  intervalId = setInterval(displayNextWord, speed);
 
-  startTime = Date.now();
-  elapsedTime = 0;
-  wordCount = 0;
-  wpm = 0;
-  updateStats();
 
-  if (timerId !== null) {
-    clearInterval(timerId);
+function startReading() {
+  const speed = document.getElementById("speed").value;
+  const words = texts[currentTextIndex].split(" ");
+  currentIndex = 0;
+  startTime = new Date();
+  
+  let countdownSeconds = 5;
+  document.getElementById("text").innerText = countdownSeconds;
+  
+  let countdownIntervalId = setInterval(() => {
+  countdownSeconds--;
+  document.getElementById("text").innerText = countdownSeconds;
+  if (countdownSeconds === 0) {
+    clearInterval(countdownIntervalId);
+    
+    intervalId = setInterval(() => {
+      if (currentIndex >= words.length) {
+        currentIndex = 0; 
+      }
+      document.getElementById("text").innerText = words[currentIndex++];
+      wordCount++;
+      document.getElementById("count").innerText = `Прочитано слов: ${wordCount}`;
+    }, 1000 * 60 / speed);
   }
-  updateTimer();
-  timerId = setInterval(updateTimer, 1000);
+}, 1000);
 
-  const wpmElement = document.querySelector(".wpm");
-  wpmElement.textContent = 0;
+timerIntervalId = setInterval(() => {
+updateTimer();
+}, 1000);
 }
 
 function stopReading() {
   clearInterval(intervalId);
-  intervalId = null;
-
-  if (timerId !== null) {
-    clearInterval(timerId);
-    timerId = null;
-  }
-
-  elapsedTime = Date.now() - startTime;
-  updateStats();
+  clearInterval(timerIntervalId); 
+  document.getElementById("start").disabled = false;
+  document.getElementById("stop").disabled = true;
   updateTimer();
+  document.getElementById("count").innerText = `Прочитано слов: ${wordCount}`;
 }
 
-function updateStats() {
-  const wordsReadElement = document.querySelector(".words-read");
-  wordsReadElement.textContent = wordCount;
-
-  if (elapsedTime > 0 && wordCount > 0) {
-    wpm = Math.round((wordCount / elapsedTime) * 60000);
-    const wpmElement = document.querySelector(".wpm");
-    wpmElement.textContent = wpm;
-  }
-}
-
-const text =
-  "скорочтение это навык быстрого чтения который позволяет прочитывать тексты на порядок быстрее чем обычным чтением люди обладающие этим навыком могут значительно увеличить свою производительность получить больше информации за короткий промежуток времени и улучшить свои умственные способности одним из главных принципов скорочтения является отказ от подсознательного замедления чтения голосом или произнесением слов в уме вместо этого скорочтец фокусируется на группах слов и предложений а не на каждом слове по отдельности одним из способов развития навыка скорочтения является увеличение скорости чтения путем уменьшения количества времени затрачиваемого на чтение каждой страницы другим способом является тренировка в восприятии групп слов улучшение внимания и скорости реакции на изменения в тексте тренировки скорочтения могут включать в себя использование специальных техник и упражнений таких как сканирование текста чтение с помощью периферийного зрения и чтение нескольких строк одновременно также можно использовать специальные программы которые помогают тренировать навык скорочтения и увеличивать скорость чтения важно понимать что скорочтение не должно приводить к потере качества восприятия информации скорочтец должен уметь понимать прочитанное и запоминать его так же как и при обычном чтении поэтому необходимо научиться контролировать скорость чтения в зависимости от сложности текста и своих возможностей интересно что некоторые известные люди были скорочтецами например Томас Джефферсон умел читать со скоростью до 1000 слов в минуту а Никола Тесла - до 1200 слов в минуту это позволяло им получать больше знаний и работать более эффективно";
-words = splitText(text);
-
-const speedButtons = document.querySelectorAll(".speed-button");
-speedButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const speed = button.dataset.speed;
-    startReading(speed);
-  });
-});
-
-const startButton = document.querySelector(".start-button");
-startButton.addEventListener("click", function () {
-  startReading(1000);
-});
-
-function resetReading() {
-  currentWordIndex = 0;
+function clearReading() {
+  clearInterval(intervalId);
+  clearInterval(timerIntervalId); 
+  document.getElementById("start").disabled = false;
+  document.getElementById("stop").disabled = true;
+  document.getElementById("text").innerText = "";
+  document.getElementById("count").innerText = "";
+  document.getElementById("timer").innerText = "Время: 0 секунд";
+  currentIndex = 0;
   wordCount = 0;
-  elapsedTime = null;
-  wpm = 0;
-  updateStats();
-
-  const wordContainer = document.querySelector(".word-container");
-  wordContainer.textContent = "";
-
-  const timerElement = document.querySelector(".timer");
-  timerElement.textContent = "0:00";
+  startTime = new Date();
 }
 
-let stopButtonClickCount = 0;
-
-const stopButton = document.querySelector(".stop-button");
-stopButton.addEventListener("click", function () {
-  stopButtonClickCount++;
-
-  if (stopButtonClickCount === 1) {
-    stopReading();
-  } else {
-    resetReading();
-    stopButtonClickCount = 0;
+function toggleText() {
+  currentTextIndex++;
+  if (currentTextIndex >= texts.length) {
+    currentTextIndex = 0;
   }
+  clearReading();
+  document.getElementById("text").innerText = "";
+}
+
+document.getElementById("start").addEventListener("click", () => {
+  document.getElementById("start").disabled = true;
+  document.getElementById("stop").disabled = false;
+  startReading();
+});
+
+document.getElementById("stop").addEventListener("click", stopReading);
+
+document.getElementById("clear").addEventListener("click", clearReading);
+
+document.getElementById("toggle").addEventListener("click", toggleText);
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("stop").disabled = true;
+  document.getElementById("timer").innerText = "Время: 0 секунд";
+  document.getElementById("text").innerText = texts[currentTextIndex];
 });

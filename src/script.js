1b3fec26c72a20fbd2826768f53708ad05a6d839
Arkadiy <woodythps@gmail.com>
2023-04-22 03:22:20 +0300

    Summary

--- src/script.js
+++ src/script.js
@@ -8,12 +8,14 @@ let countdownIntervalId;
 let countdownValue = 3;
 
 const texts = ["скорочтение это навык быстрого чтения который позволяет прочитывать тексты на порядок быстрее чем обычным чтением люди обладающие этим навыком могут значительно увеличить свою производительность получить больше информации за короткий промежуток времени и улучшить свои умственные способности одним из главных принципов скорочтения является отказ от подсознательного замедления чтения голосом или произнесением слов в уме вместо этого скорочтец фокусируется на группах слов и предложений а не на каждом слове по отдельности одним из способов развития навыка скорочтения является увеличение скорости чтения путем уменьшения количества времени затрачиваемого на чтение каждой страницы другим способом является тренировка в восприятии групп слов улучшение внимания и скорости реакции на изменения в тексте тренировки скорочтения могут включать в себя использование специальных техник и упражнений таких как сканирование текста чтение с помощью периферийного зрения и чтение нескольких строк одновременно также можно использовать специальные программы которые помогают тренировать навык скорочтения и увеличивать скорость чтения важно понимать что скорочтение не должно приводить к потере качества восприятия информации скорочтец должен уметь понимать прочитанное и запоминать его так же как и при обычном чтении поэтому необходимо научиться контролировать скорость чтения в зависимости от сложности текста и своих возможностей интересно что некоторые известные люди были скорочтецами например Томас Джефферсон умел читать со скоростью до 1000 слов в минуту а Никола Тесла - до 1200 слов в минуту это позволяло им получать больше знаний и работать более эффективно", 
-"Sit amet consectetur adipiscing elit", "aaa ddd aaa"];
+"Sit amet consectetur adipiscing elit", "aaa ddd aaa", "eba eba eba", "da net da net"];
 
 const textTitles = {
   0: "Скорочтение",
   1: "Lorem Ipsum",
-  2: "AAA DDD AAA"
+  2: "Lerrarara",
+  3: "Eba",
+  4: "Da da net"
 };
 
 
@@ -87,34 +89,42 @@ function clearReading() {
 }
 
 function selectText() {
-  const select = document.createElement("select");
-  select.setAttribute("id", "select-text");
+  // Создаем группу кнопок для выбора текста
+  const group = document.createElement("div");
+  group.setAttribute("id", "select-text");
 
-  // Добавляем опции выбора текста
+  // Добавляем кнопки выбора текста
   for (let i = 0; i < texts.length; i++) {
-    const option = document.createElement("option");
-    option.value = i;
-    option.text = textTitles[i];
-    select.appendChild(option);
+    const button = document.createElement("button");
+    button.setAttribute("data-index", i);
+    button.classList.add("text-selector-button");
+    button.innerText = textTitles[i];
+    group.appendChild(button);
   }
 
   // Создаем модальное окно
   const modal = document.createElement("div");
+  const toggleButton = document.getElementById("toggle");
   modal.classList.add("modal");
-  modal.appendChild(select);
-  const button = document.createElement("button");
-  button.innerText = "Выбрать";
-  button.addEventListener("click", () => {
-    currentTextIndex = select.value;
-    modal.remove();
-    clearReading();
-    document.getElementById("selected-text").innerText = "Выбран текст: " + textTitles[currentTextIndex];
-    toggleButton.disabled = false; // Разблокировка кнопки toggle
-  });
-  modal.appendChild(button);
-  document.body.appendChild(modal);
+  modal.appendChild(group);
 
-  // Удаление модального окна при клике за его пределами
+  // Функция выбора текста
+  const selectTextHandler = (event) => {
+    const button = event.target;
+    if (button.tagName === "BUTTON") {
+      currentTextIndex = parseInt(button.getAttribute("data-index"), 10);
+      modal.remove();
+      clearReading();
+      document.getElementById("selected-text").innerText = "Выбран текст: " + textTitles[currentTextIndex];
+      toggleButton.disabled = false;
+      group.removeEventListener("click", selectTextHandler); // Удаляем обработчик "click"
+    }
+  };
+
+  group.addEventListener("click", selectTextHandler);
+
+  toggleButton.parentNode.appendChild(modal);
+
   modal.addEventListener("click", (event) => {
     if (event.target === modal) {
       modal.remove();
@@ -122,13 +132,11 @@ function selectText() {
     }
   });
 
-  const toggleButton = document.getElementById("toggle");
-  toggleButton.disabled = true; // Блокировка кнопки toggle
-  document.getElementById("selected-text").innerText = "Сначала выберите текст";
+  toggleButton.disabled = true;
+  document.getElementById("selected-text").innerText = "Выберите текст:";
 }
 
-
-
+// Обработчик нажатия на кнопку "Выбрать текст"
 document.getElementById("toggle").addEventListener("click", selectText);
 document.getElementById("start").addEventListener("click", () => {
   startCountdown();
